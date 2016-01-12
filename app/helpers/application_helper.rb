76dbf2bff556253edb4466e1ca082cf51aa1206d
Kunal Kamath <kkamath101@gmail.com>
2016-01-12 13:14:05 +0530

    Added a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base = "RoR Sample App"
+    if page_title.empty?
+      base
+    else
+      page_title + " | " + base
+    end
+  end
 end
